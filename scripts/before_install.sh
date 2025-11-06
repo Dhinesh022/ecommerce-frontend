99b752114c7939c32e1cf18939b907ed5077abf4
DHINESH H <Dhineshbaskar02@gmail.com>
2025-11-06 15:19:25 +0530

    Implement cleanup script in before_install.sh
    Add cleanup script to remove all files except specified folders.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+set -e
+
 cd /var/www/E-Commerce-react-node-mysql
-rm -rf * 
+
+echo "Cleaning /var/www/E-Commerce-react-node-mysql except specified folders..."
+
+for item in *; do
+  case "$item" in
+    music|games|test)
+      echo "Skipping $item"
+      ;;
+    *)
+      echo "Deleting $item"
+      sudo rm -rf "$item"
+      ;;
+  esac
+done
+
+echo "Cleanup complete"
